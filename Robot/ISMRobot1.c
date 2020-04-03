b7dcf13a894fc1cdf59506248decd7e8010b0c98
jessiexiong <45522871+jessiexiong@users.noreply.github.com>
2020-04-03 11:38:25 -0400

    Update ISMRobot1.c

--- Robot/ISMRobot1.c
+++ Robot/ISMRobot1.c
@@ -1,16 +1,23 @@
+//IR sensor implementation
+    #include "QSerial.h"
+    QSerial myIRx;
+    QSerial myIRy;
+
+//ISM transmitter and receiver implementation
     #include <SPI.h>
     #include <nRF24L01.h>
     #include <RF24.h>
-    
     RF24 radio(9, 10); // CE, CSN
-
     //the address or pipe the two arduinos will use to communicate
     //chooses which reciever to talk to 
     const byte address[6] = "00001";
-    boolean button_state = 0;
-    int led_pin = 3;
     
     void setup() {
+//IR sensor setup
+    myIRx.attach(5,-1);
+    myIRy.attach(4,-1);
+
+//ISM transmitter and receiver setup
         pinMode(6, OUTPUT);
         Serial.begin(9600);
         //Initiate transmission signals
@@ -22,7 +29,11 @@
     }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
+  //once IR sensors have received information, store in array
+  char IRlocationx = myIRx.receive(500);
+  char IRlocationy = myIRx.receive(500);
+  char location[2] = IRlocationx,IRlocationy;
+  //use ISM transciever and send to robot 2
+  radio.write(&location, sizeof(location));
   delay(1000);
 }
