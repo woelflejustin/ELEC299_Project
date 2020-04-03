734fae1a6c3aca9ce060c24caf9a36bae6c1475e
jessiexiong <45522871+jessiexiong@users.noreply.github.com>
2020-04-03 11:17:15 -0400

    Update ISMRobot3.c

--- Robot/ISMRobot3.c
+++ Robot/ISMRobot3.c
@@ -1,34 +1,39 @@
-    #include <SPI.h>
-    #include <nRF24L01.h>
-    #include <RF24.h>
-    RF24 radio(9, 10); // CE, CSN
-    const byte address[6] = "00001";
-    boolean button_state = 0;
-    int led_pin = 3;
+ //IR sensor implementation
+        #include "QSerial.h"
+        QSerial myIRx;
+        QSerial myIRy;
+
+//ISM transmitter and receiver implementation
+        #include <SPI.h>
+        #include <nRF24L01.h>
+        #include <RF24.h>
+        RF24 radio(9, 10); // CE, CSN
+        const byte address[6] = "00001";
+
     void setup() {
-    pinMode(6, OUTPUT);
-    Serial.begin(9600);
-    radio.begin();
-    radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
-    radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
-    radio.startListening();              //This sets the module as receiver
+ //IR sensor setup
+        myIRx.attach(5,-1);
+        myIRy.attach(4,-1);
+//ISM transmitter and receiver setup
+        Serial.begin(9600);
+        radio.begin();
+        radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
+        radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
+        radio.startListening();              //This sets the module as receiver
     }
     void loop()
     {
-        if (radio.available())              //Looking for the data.
-        {
-        char text[32] = "";                 //Saving the incoming data
-        radio.read(&text, sizeof(text));    //Reading the data
-        radio.read(&button_state, sizeof(button_state));    //Reading the data
-            if(button_state == HIGH)
-            {
-            digitalWrite(6, HIGH);
-            Serial.println(text);
-            }
-            else
-            {
-            digitalWrite(6, LOW);
-            Serial.println(text);}
-            }
-    delay(5);
+//IR sensors receive robot location
+      char IRlocationx = myIRx.receive(500);
+      char IRlocationy = myIRx.receive(500);
+//ISM transceiver recieves information from robot 1
+      delay(5);
+      radio.startListening();                            //This sets the module as receiver
+      while(!radio.available());                         //Looking for incoming data
+      radio.read(&location, sizeof(location)); //Reading the data
+  //driving code in direction of location[]
+      int vectorx = x - IRlocationx;
+        //driving code in direction of vectorx
+      int vectorx = x - IRlocationx;
+        //driving code in direction of vectorx
     }
